0813371b649b11c5a79ff93dd07baf8822007e8b
CleberAugustz <cleber.augustz@hotmail.com>
2020-09-05 09:55:28 -0300

    Rename ex07.py to 07 - Operation_on_format.py

--- ex07.py
+++
@@ -1,3 +0,0 @@
-b = int(input('Digite o valor da base do quadrado:\n'))
-h = int(input('Digite o valor da altura do quadrado:\n'))
-print('a área do quadrado com base {} e altura {} é equivalente a {}\n o dobro dessa área é {}'.format(b,h,b*h,(b*h)*2))
