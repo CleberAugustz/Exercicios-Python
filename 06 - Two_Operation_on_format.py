606d8167c1d5a519e4e52ca573d2f621001faa5a
CleberAugustz <cleber.augustz@hotmail.com>
2020-09-05 09:54:51 -0300

    Rename ex06.py to 06 - Two_Operation_on_format.py

---
+++ 06 - Two_Operation_on_format.py
@@ -0,0 +1,2 @@
+r = int(input('Digite valor do raio de um círculo:\n'))
+print('A área do circulo com raio {}, é {}'.format(r,(r**2)*3.14))
--- ex06.py
+++
@@ -1,2 +0,0 @@
-r = int(input('Digite valor do raio de um círculo:\n'))
-print('A área do circulo com raio {}, é {}'.format(r,(r**2)*3.14))
