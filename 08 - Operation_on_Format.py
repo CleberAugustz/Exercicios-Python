39b5353894d5f7e2a8194a6d5dad2fd2cb0cac8d
CleberAugustz <cleber.augustz@hotmail.com>
2020-09-05 09:56:33 -0300

    Rename ex08.py to 08 - Operation_on_Format.py

---
+++ 08 - Operation_on_Format.py
@@ -0,0 +1,3 @@
+sh = int(input('Quanto você ganha por Hora?\n'))
+ht = int(input('Quantas horas você trabalha?\n'))
+print('Você trabalha {} Horas. Você ganha R${} por Hora.\nSeu salário no mês é R${}'.format(ht,sh,ht*sh))
